ca70161241b199ddbbb4afd6b5d1d5476e7c2858
john sandstedt <john sandstedt>
2016-07-04 11:51:26 -0400

    added a placeholder icon for when the icon is replaced

--- remove schoology icons.user.js
+++ remove schoology icons.user.js
@@ -2,15 +2,21 @@
 // @name         remove schoology icons
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  get rid of mr. coxes shart icon
-// @author       You
+// @description  get rid of annoying schoology icons
+// @author       Aggendo (John Sandstedt)
 // @match        https://app.schoology.com/*
 // @grant        none
 // ==/UserScript==
 
 //CONFIG
-//add the url that point to the annoying images src
+//add the urls that point to the annoying images src
+//current url is to mr. Coxes annoying shark icon
 var iconSrcs = ["https://cdn3-4.cdn.schoology.com/system/files/imagecache/profile_reg/courselogos/logo-595068063_574ed5072658d.gif?1464784135"];
+var replaceIcon = true;
+//needs to be 170x170
+var urlForReplacementImg = "http://img.clubic.com/00AA000005414097-photo-adblock-plus-adb-logo-sq-gb.jpg";
+
+//END CONFIG
 
 function arrayContains(needle, arrhaystack)
 {
@@ -23,7 +29,11 @@ function arrayContains(needle, arrhaystack)
         var img = imgs[i];
         console.log(img.src);
         if(arrayContains(img.src, iconSrcs)){
-            img.parentNode.removeChild(img);
+			if(replaceIcon){
+				img.src = urlForReplacementImg;
+			} else {
+            	img.parentNode.removeChild(img);
+			}
         }
     }
 
